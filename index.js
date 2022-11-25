561fa41b2e8cd17104ae65d41aed225adacecc95
Md Ashik Billah <ashikbillah699@gmail.com>
2022-11-25 21:45:42 +0600

    categorys data load

--- index.js
+++ index.js
@@ -3,13 +3,38 @@ const cors = require('cors')
 const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.rhwxyri.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const categoryCollection = client.db('laptopCorder').collection('laptopData')
+        const categorydataCollection = client.db('laptopCorder').collection('categorydata')
+
+        app.get('/categorys', async (req, res) => {
+            const query = {};
+            const cursor = categoryCollection.find(query);
+            const categorys = await cursor.toArray();
+            res.send(categorys);
+        })
+
+        app.get('/category/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const category = await categoryCollection.findOne(query);
+            res.send(category)
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
